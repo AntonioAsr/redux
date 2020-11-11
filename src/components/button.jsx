36cca75b3a0a96e149723e2273853df31662f3f0
= <antonio.segovia@iris.net>
2020-11-11 16:13:32 +0000

    remove slash

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Button extends Component {
 
     render() {
-        return <p className='title'>/View 1</p>
+        return <p className='title'>View 1</p>
     }
     
 }
